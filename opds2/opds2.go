2d8defcec11226508c209e120be56a6a17e46a9b
ohzqq <iamchurkey@gmail.com>
2023-03-10 16:10:39 -0500

    stuff

--- opds2/opds2.go
+++ opds2/opds2.go
@@ -51,17 +51,17 @@ type Group struct {
 
 // Link object used in collections and links
 type Link struct {
-	Href       string        `json:"href"`
-	TypeLink   string        `json:"type,omitempty"`
-	Rel        StringOrArray `json:"rel,omitempty"`
-	Height     int           `json:"height,omitempty"`
-	Width      int           `json:"width,omitempty"`
-	Title      string        `json:"title,omitempty"`
-	Properties *Properties   `json:"properties,omitempty"`
-	Duration   string        `json:"duration,omitempty"`
-	Templated  bool          `json:"templated,omitempty"`
-	Children   []Link        `json:"children,omitempty"`
-	Bitrate    int           `json:"bitrate,omitempty"`
+	Href       string      `json:"href"`
+	TypeLink   string      `json:"type,omitempty"`
+	Rel        []string    `json:"rel,omitempty"`
+	Height     int         `json:"height,omitempty"`
+	Width      int         `json:"width,omitempty"`
+	Title      string      `json:"title,omitempty"`
+	Properties *Properties `json:"properties,omitempty"`
+	Duration   string      `json:"duration,omitempty"`
+	Templated  bool        `json:"templated,omitempty"`
+	Children   []Link      `json:"children,omitempty"`
+	Bitrate    int         `json:"bitrate,omitempty"`
 }
 
 // Properties object use to link properties
@@ -102,7 +102,7 @@ type PublicationMetadata struct {
 	Contributor     []Contributor `json:"contributor,omitempty"`
 	Publisher       []Contributor `json:"publisher,omitempty"`
 	Imprint         []Contributor `json:"imprint,omitempty"`
-	Language        StringOrArray `json:"language,omitempty"`
+	Language        []string      `json:"language,omitempty"`
 	Modified        *time.Time    `json:"modified,omitempty"`
 	PublicationDate *time.Time    `json:"published,omitempty"`
 	Description     string        `json:"description,omitempty"`
